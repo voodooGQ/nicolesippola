0c9d3cb3236e457265a1b08c1aae264ec91ee433
Shane Smith <ssmith@nerdery.com>
2016-09-18 12:36:46 -0500

    Refactoring scss gulp task

--- build/gulpfile.js
+++ build/gulpfile.js
@@ -1,22 +1,30 @@
 'use strict';
 
-var ASSET_FOLDER = '../public/content/themes/nicolesippola/assets/';
-var SCSS_SRC = ['./styles/screen.scss'];
-
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
-    minifyCss = require('gulp-minify-css'),
+    minify = require('gulp-minify-css'),
     concat = require('gulp-concat'),
-    rename = require('gulp-rename');
+    rename = require('gulp-rename'),
+    argv = require('yargs').argv,
+    env = require('./env');
 
+var ASSET_FOLDER = env.DIR_THEME + 'assets/';
+var SCSS_SRC = ['./' + env.DIR_SRC + 'styles/screen.scss'];
+
+// SCSS Compiation
 gulp.task('scss', function() {
-    gulp.src(SCSS_SRC)
+    var compiled = gulp.src(SCSS_SRC)
         .pipe(sass().on('error', sass.logError))
-        // .pipe(minifyCss())
-        .pipe(concat('screen.css'))
-        .pipe(rename({
-            basename: 'screen',
-            extname: '.min.css'
-        }))
-        .pipe(gulp.dest(ASSET_FOLDER + 'styles'));
+        .pipe(concat('screen.css'));
+
+    // Minify if production
+    if(argv.prod) {
+        compiled.pipe(minify())
+            .pipe(rename({
+                basename: 'screen',
+                extname: '.min.css'
+            }));
+    }
+
+    compiled.pipe(gulp.dest(ASSET_FOLDER + 'styles'));
 });
